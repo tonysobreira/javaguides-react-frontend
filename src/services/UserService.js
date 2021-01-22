ab29975525814c4618743c94820ba90af3b5ee72
Tony Sobreira <tonysobreira@gmail.com>
2021-01-22 16:06:49 -0300

    heroku backend

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL = 'http://localhost:8080/api/users';
+//const USERS_REST_API_URL = 'http://localhost:8080/api/users';
+const USERS_REST_API_URL = 'https://javaguides-springboot-backend.herokuapp.com/api/users';
 
 class UserService {
 
     getUsers(){
         return axios.get(USERS_REST_API_URL);
     }
+
 }
 
 export default new UserService();
